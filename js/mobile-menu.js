1341177d5536de08f4be282f2fc254ddc37f2599
olesyamaniuk <maniukolesya222@gmail.com>
2024-12-25 13:01:40 +0200

    2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -21,18 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
   );
 });
 
-function toggleAnswer(element) {
-  const answer = element.nextElementSibling;
-  const buttonImg = element.querySelector(".toggle-button img"); 
-
-  if (answer.style.display === "none" || answer.style.display === "") {
-    answer.style.display = "block";
-    buttonImg.src = "/img/faq/Component 1.png"; 
-  } else {
-    answer.style.display = "none";
-    buttonImg.src = "/img/faq/Component 1.png"; 
-  }
-}
 
 
 
--- js/slick.js
+++ js/slick.js
@@ -7,7 +7,7 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     speed: 1000,
-    dots: false,
+    dots: true,
     centerMode: false,
     cssEase: 'linear',
     prevArrow: '.gallery__list-left',
@@ -24,27 +24,12 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     speed: 1000,
-    dots: false,
+    dots: true,
     centerMode: true,
     cssEase: 'linear',
     prevArrow: '.play__list-left',
     nextArrow: '.play__list-right',
   });
 });
-$(function () {
-  $('.gallery-main-list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: false,
-    centerMode: true,
-    cssEase: 'linear',
-    prevArrow: '.gallery-main__list-left',
-    nextArrow: '.gallery-main__list-right',
-  });
-});
+
 
